3b63ff13edbc1374b1a14b1cc016b0ab6a80aad9
Abbas Pardhan <abbas.pardhan@theaccessgroup.com>
2021-05-23 00:01:20 +0100

    Initial Commit

---
+++ src/main/java/com/example/crimedatarest/CrimeDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.crimedatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimeDataRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crimedatarest/CrimeDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crimedatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
